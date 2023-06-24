dbe61d55f16925151b0c4e5f1225da639d8126e6
RUSHHE <2433323047@qq.com>
2023-06-24 19:17:21 +0800

    Finish Blanks programming4

---
+++ Midterm exam/Blanks Programming/Blanks Programming4/src/SumDemo.java
@@ -0,0 +1,12 @@
+/**
+ * 【程序填空】关于求和运行结果是     10050      【 正确答案: 10050】
+ */
+public class SumDemo {
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 51; i < 151; i++) {
+            a += i;
+        }
+        System.out.print(a);
+    }
+}
