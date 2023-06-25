2aa8624145a852a6b06236d3711507c53b1f3cd4
RUSHHE <2433323047@qq.com>
2023-06-25 12:50:33 +0800

    Experiment 1, square: Compact the codes

--- Experiment/1/Square/src/Main.java
+++ Experiment/1/Square/src/Main.java
@@ -1,11 +1,8 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        double l,w;//l是长，w是宽
         Scanner scan = new Scanner(System.in);
-        l = scan.nextDouble();
-        w = scan.nextDouble();
-        double square = l*w;
-        System.out.printf("The area is:%.2f",square);
+
+        System.out.printf("The area is:%.2f",scan.nextDouble() * scan.nextDouble());
     }
 }
