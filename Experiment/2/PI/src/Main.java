c6a62ba2da196da114ed34786486f54e411a914d
RUSHHE <2433323047@qq.com>
2023-06-25 14:20:51 +0800

    Experiment 2, PI: Compact the codes && Format the codes

--- Experiment/2/PI/src/Main.java
+++ Experiment/2/PI/src/Main.java
@@ -3,6 +3,7 @@
  * pi=4*（1-1/3+1/5-1/7+1/9-1/11+1/13+...+1/(2n-1))
  * 要求使用循环计算n不同值时的pi的值。
  */
+
 import java.util.Scanner;
 
 public class Main {
@@ -12,18 +13,20 @@ public class Main {
         for (int i = 1; i <= n; i++) {
             pi += Math.pow(-1, i + 1) * 1 / (2 * i - 1);
         }
-        pi *= 4;
-        return pi;
+        return pi * 4;
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Please input a number(10000-100000):");
         int n = scan.nextInt();
+
         if (!(n >= 10000 && n <= 100000)) {
             System.out.println("The input data is out of range!");
             System.exit(1);
         }
+
         System.out.println("PI=" + cal_Pi(n));
     }
 }
